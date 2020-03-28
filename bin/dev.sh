c65a13834ab9f25fc3daa9cb4f81ecb22f11026f
Christian Mayer <christian@fox21.at>
2020-03-28 16:48:29 +0100

    Main program layout.

--- bin/dev.sh
+++ bin/dev.sh
@@ -2,7 +2,6 @@
 
 SCRIPT_BASEDIR=$(dirname "$0")
 
-which rustc &> /dev/null || { echo 'ERROR: rustc not found in PATH'; exit 1; }
 which cargo &> /dev/null || { echo 'ERROR: cargo not found in PATH'; exit 1; }
 
 cd "${SCRIPT_BASEDIR}/.."
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,50 @@
 
+extern crate clap;
+use clap::{App, Arg, ArgMatches};
+use std::env::args;
+
+const APP_NAME: &'static str = env!("CARGO_PKG_NAME");
+// const APP_NAME: &'static str = "ParametersRust";
+const APP_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const APP_AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
+const APP_HOMEPAGE: &'static str = env!("CARGO_PKG_HOMEPAGE");
+
 fn main() {
-    println!("Hello Parameters");
+    println!("-> start");
+
+    let args: Vec<String> = args().collect();
+    println!("-> args: '{:?}'", args);
+
+    // Vars Sub Command
+    let vars_subcmd = App::new("vars")
+        .about("Print variables.");
+
+    // Main App
+    let app = App::new(APP_NAME)
+        .version(APP_VERSION)
+        .author(APP_AUTHORS)
+        .about(APP_HOMEPAGE)
+        .usage("parameters [OPTIONS] [SUBCOMMAND]")
+        .subcommand(vars_subcmd);
+    
+    // Get Argument matches.
+    let matches = app.get_matches();
+    println!("-> matches '{:?}'", matches);
+
+    match matches.subcommand() {
+        ("vars", _) => {
+            println!("-> cmd: vars");
+            println!("APP_NAME '{}'", APP_NAME);
+            println!("APP_VERSION '{}'", APP_VERSION);
+
+            return;
+        },
+        _ => {
+            println!("No command.");
+        },
+    }
+
+    println!("-> end");
 }
 
 #[cfg(test)]
