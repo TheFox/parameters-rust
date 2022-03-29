19af847b9d7585693d3044468d49ba5384c6c89c
Christian Mayer <christian@fox21.at>
2022-03-29 15:20:35 +0200

    Version 0.2.3

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,6 +7,9 @@ which cargo &> /dev/null || { echo 'ERROR: cargo not found in PATH'; exit 1; }
 cd "${SCRIPT_BASEDIR}/.."
 pwd
 
+git status
+git diff
+
 set -x
-cargo publish --package parameters
-cargo publish --package parameters_lib
+cargo publish --allow-dirty --package parameters_lib
+cargo publish --allow-dirty --package parameters
